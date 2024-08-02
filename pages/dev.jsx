7a6b979745b01756135b86dd395292f501bcf85d
Julia Bauer <juliabauer@ufl.edu>
2024-08-02 00:39:21 -0400

    update dev pg

--- pages/dev.jsx
+++ pages/dev.jsx
@@ -19,6 +19,8 @@ export default function Dev() {
             <img style={{ width: "250px", height: "auto"}} src="/dev.png" alt="Development icon" />
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam, inventore dolore cupiditate blanditiis distinctio temporibus non. Deserunt velit eum eveniet sapiente eius quaerat fugiat sit odit quisquam. Laboriosam, eligendi dolorum. Lorem ipsum dolor, sit amet consectetur adipisicing elit. Debitis non voluptatem ad delectus. Dolore architecto, deserunt ratione vel, magnam ducimus soluta sint quo, earum quos consequatur iure sequi ab repudiandae.</p>
             <h2>Capstone Project</h2>
+            <h2>Portfolio Site</h2>
+            <p>(This website!)</p>
             <h2>Web 3 Project</h2>
             <h2>Web 1 Project</h2>
             <h2>First Project</h2>
