376dd66156824e907e3d328f415870ae391a292c
Julia Bauer <juliabauer@ufl.edu>
2024-08-06 08:38:31 -0400

    footer fix

--- components/Footer/index.js
+++ components/Footer/index.js
@@ -9,11 +9,11 @@ export default function Footer() {
     return (
         <footer className={styles.container}>
             <div className={styles.linkDiv}>
-                <a href="https://www.linkedin.com/in/julia-bauer-896229201/" target="_blank">
-                    <p>LinkedIn</p>
-                </a>
                 <a href="https://github.com/juliacbauer" target="_blank">
                     <p>GitHub</p>
+                </a>
+                <a href="https://www.linkedin.com/in/julia-bauer-896229201/" target="_blank">
+                    <p>LinkedIn</p>
                 </a>
                 <btn onClick={handleEmailClick} style={{ cursor: "pointer" }}>
                     <p className={styles.btn}>Email</p>
