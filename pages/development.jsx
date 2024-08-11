b60ff01b2e8c510d07a4fbd92fc57dc9803a3fee
Julia Bauer <juliabauer@ufl.edu>
2024-08-11 13:13:59 -0400

    dev images

--- pages/development.jsx
+++ pages/development.jsx
@@ -30,29 +30,62 @@ export default function Dev() {
                 <h1 className={styles.h1}>Code</h1>
                 <LazyLoadedIcon className={styles.logo} src="/devicon.webp" alt="Development icon" />
               </div>
-              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam, inventore dolore cupiditate blanditiis distinctio temporibus non. Deserunt velit eum eveniet sapiente eius quaerat fugiat sit odit quisquam. Laboriosam, eligendi dolorum. Lorem ipsum dolor, sit amet consectetur.</p>
+              <div className={styles.pDiv}>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam, inventore dolore cupiditate blanditiis distinctio temporibus non. Deserunt velit eum eveniet sapiente eius quaerat fugiat sit odit quisquam. Laboriosam, eligendi dolorum. Lorem ipsum dolor, sit amet consectetur.</p>
+              </div>
+            </ScrollTriggeredSection>
+            <ScrollTriggeredSection>
+              <h2>Cinememo</h2>
+              <div className={styles.bgDiv}>
+                <div className={styles.workImgDiv}>
+                  <Link href="https://cinememo.vercel.app/" target="_blank">
+                    <img className={`${styles.bob} ${styles.workImg}`} src="/cinememo.webp" alt="Cinememo web app" />
+                  </Link>
+                  <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quibusdam cumque nulla, optio aut quod atque repellendus sunt deleniti. Voluptatem eligendi pariatur vitae obcaecati repellendus delectus aliquid vel eaque placeat nisi!</p>
+                </div>
+              </div>
+            </ScrollTriggeredSection>
+            <div className={styles.spacer} />
+            <ScrollTriggeredSection>
+              <img className={styles.bannerImg} src="/cinememoBanner.webp" alt="Cinememo web app" />
             </ScrollTriggeredSection>
             <div className={styles.smallSpacer} />
             <ScrollTriggeredSection>
-              <h2>Cinememo</h2>
-              <p>Capstone Project</p>
+              <h2>Prose Pal</h2>
+              <div className={styles.bgDiv}>
+                <div className={styles.workImgDiv}>
+                  <Link href="https://prose-pal-julia-bauer.vercel.app/" target="_blank">
+                    <img className={`${styles.bob} ${styles.workImg}`} src="/prosepal.webp" alt="Prose Pal web app" />
+                  </Link>
+                  <p>This website! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quibusdam cumque nulla, optio aut quod atque repellendus sunt deleniti. Voluptatem eligendi pariatur vitae obcaecati repellendus delectus aliquid vel eaque placeat nisi!</p>
+                </div>
+              </div>
+            </ScrollTriggeredSection>
+            <div className={styles.spacer} />
+            <ScrollTriggeredSection>
+              <img className={styles.bannerImg} src="/prosepalBanner.webp" alt="Prose Pal web app" />
             </ScrollTriggeredSection>
             <div className={styles.smallSpacer} />
             <ScrollTriggeredSection>
               <h2>Portfolio Site</h2>
-              <p>(This website!)</p>
-            </ScrollTriggeredSection>
-            <div className={styles.smallSpacer} />
-            <ScrollTriggeredSection>
-              <h2>Web 3 Project</h2>
-            </ScrollTriggeredSection>
-            <ScrollTriggeredSection>
-              <p>ProsePal</p>
+              <div className={styles.bgDiv}>
+                <div className={styles.workImgDiv}>
+                  <Link href="https://github.com/juliacbauer/juliacbauer" target="_blank">
+                    <img className={`${styles.bob} ${styles.workImg}`} src="/finalpersonallogo.webp" alt="Portfolio site" />
+                  </Link>
+                  <p>This website! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quibusdam cumque nulla, optio aut quod atque repellendus sunt deleniti. Voluptatem eligendi pariatur vitae obcaecati repellendus delectus aliquid vel eaque placeat nisi!</p>
+                </div>
+                <div className={styles.pDiv}>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo, praesentium, laborum omnis velit obcaecati officiis nisi exercitationem voluptates excepturi, tempora laudantium? Optio tenetur enim porro laborum ullam fuga, natus neque!</p>
+              </div>
+              </div>
             </ScrollTriggeredSection>
             <div className={styles.smallSpacer} />
             <ScrollTriggeredSection>
               <h2>GitHub</h2>
-              <p>Feel free to view more skill-based work on my GitHub. Chronologically, my repositories illustrate my web development learning process via the Web Design and Online Communication program within the Univeristy of Florida's MA in Mass Communication degree.</p>
+              <div className={styles.pDiv}>
+                <p>Feel free to view more skill-based work on my GitHub. Chronologically, my repositories illustrate my web development learning process via the Web Design and Online Communication program within the Univeristy of Florida's MA in Mass Communication degree.</p>
+              </div>
               <div className={styles.spacer} />
               <div className={styles.btnDiv}>
                 <Link href="https://github.com/juliacbauer" target="_blank">
