3d114513e3633ac2ffe068208df6aae4be281457
Julia Bauer <juliabauer@ufl.edu>
2024-08-13 14:51:17 -0400

    CLS test

--- components/LazyLoad/index.jsx
+++ components/LazyLoad/index.jsx
@@ -1,16 +1,23 @@
-import React from 'react';
-import { LazyLoadImage } from 'react-lazy-load-image-component';
-import 'react-lazy-load-image-component/src/effects/blur.css';
+
+
+import { useState } from "react";
+import { LazyLoadImage } from "react-lazy-load-image-component";
+import "react-lazy-load-image-component/src/effects/blur.css";
 import styles from "./LazyLoad.module.css";
 
-export default function LazyLoadedImage({ src, alt, className }) {
+export default function LazyLoadedMain({ src, alt, className }) {
+  const [isLoaded, setIsLoaded] = useState(false);
+
   return (
-    <div className={styles.imgContainer}>
+    <div className={`${styles.container} ${className}`}>
       <LazyLoadImage
         alt={alt}
         src={src}
+        width="100%"
+        height="auto"
+        className={`${styles.image} ${isLoaded ? styles.loaded : ''}`}
+        onLoad={() => setIsLoaded(true)}
         effect="blur"
-        className={className}
       />
     </div>
   );
