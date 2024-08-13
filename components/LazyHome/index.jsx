a652856dc87a89cf2cebacfbba6da35f886fccc1
Julia Bauer <juliabauer@ufl.edu>
2024-08-13 15:19:43 -0400

    CLS test 4

--- components/LazyHome/index.jsx
+++ components/LazyHome/index.jsx
@@ -1,25 +1,16 @@
-import { useState } from "react";
-import { LazyLoadImage } from "react-lazy-load-image-component";
-import "react-lazy-load-image-component/src/effects/blur.css";
+import React from 'react';
+import { LazyLoadImage } from 'react-lazy-load-image-component';
+import 'react-lazy-load-image-component/src/effects/blur.css';
 import styles from "./LazyHome.module.css";
 
-export default function LazyLoadedMain({ src, alt, className, width, height }) {
-  const [isLoaded, setIsLoaded] = useState(false);
-  const aspectRatio = height / width;
-
+export default function LazyLoadedHome({ src, alt, className }) {
   return (
-    <div
-      className={`${styles.container} ${className}`}
-      style={{ aspectRatio: `${width} / ${height}` }}
-    >
+    <div className={styles.imgContainer}>
       <LazyLoadImage
         alt={alt}
         src={src}
-        width="100%"
-        height="auto"
-        className={`${styles.image} ${isLoaded ? styles.loaded : ''}`}
-        onLoad={() => setIsLoaded(true)}
         effect="blur"
+        className={className}
       />
     </div>
   );
