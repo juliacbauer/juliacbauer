e3fff43878d24668aca76ef5d8feff6e9cfce526
Julia Bauer <juliabauer@ufl.edu>
2024-08-13 15:04:06 -0400

    CLS test 3

--- components/LazyHome/index.jsx
+++ components/LazyHome/index.jsx
@@ -3,11 +3,15 @@ import { LazyLoadImage } from "react-lazy-load-image-component";
 import "react-lazy-load-image-component/src/effects/blur.css";
 import styles from "./LazyHome.module.css";
 
-export default function LazyLoadedMain({ src, alt, className }) {
+export default function LazyLoadedMain({ src, alt, className, width, height }) {
   const [isLoaded, setIsLoaded] = useState(false);
+  const aspectRatio = height / width;
 
   return (
-    <div className={`${styles.container} ${className}`}>
+    <div
+      className={`${styles.container} ${className}`}
+      style={{ aspectRatio: `${width} / ${height}` }}
+    >
       <LazyLoadImage
         alt={alt}
         src={src}
