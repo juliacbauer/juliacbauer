0e93d82b6149899f86aaf039681e90213469da15
Julia Bauer <juliabauer@ufl.edu>
2025-03-10 22:58:39 -0400

    typo

--- pages/adcampaign.jsx
+++ pages/adcampaign.jsx
@@ -20,7 +20,7 @@ export default function AdCampaign() {
                         <WorkPage
                             title="Travel Vida Social Media Campaign"
                             paragraphs={[
-                                "This social media ad campaign was an exercise for my Photoshop class. The ficitonal company, Travel Vida, had a target audience of young adults and wanted to appeal to group travelers and solo travelers looking to meet people. The company wanted to create a unified, cohesive social media ad campaign across Instagram and Facebook.",
+                                "This social media ad campaign was an exercise for my Photoshop class. The fictional company, Travel Vida, had a target audience of young adults and wanted to appeal to group travelers and solo travelers looking to meet people. The company wanted to create a unified, cohesive social media ad campaign across Instagram and Facebook.",
                                 "I chose photos I felt would appeal to young adults with a focus on the togetherness aspect of the company, and I used the company's logo as inspiration for my color choices. However, I felt like the imagery and copy I wrote was not enough. So, I designed a shape accent that I used across the ads, varying the colors depending on the photo.",
                             ]}
                             images={[
